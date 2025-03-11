3aa6a26af74e2cf97104864d53672cb660633b78
Yoseff2550 <yokkumchu@gmail.com>
2025-03-11 10:28:03 +0700

    Add files via upload

---
+++ cell02/Ex20/iszero.py
@@ -0,0 +1,10 @@
+print ("This number is equal to zero.")
+def main():
+    number = float(input("Please enter a number: "))
+    if number == 0:
+        print("This number is equal to zero.")
+    else:
+        print("This number is different from zero.")
+
+if __name__ == "__main__":
+    main()

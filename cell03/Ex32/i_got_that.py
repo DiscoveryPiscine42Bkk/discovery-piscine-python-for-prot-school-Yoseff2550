008a9fd2621ef98f10723afff0c4683967e2ec4a
Yoseff2550 <yokkumchu@gmail.com>
2025-03-11 12:00:11 +0700

    Add files via upload

---
+++ cell03/Ex32/i_got_that.py
@@ -0,0 +1,8 @@
+Q = str(input('What you gotta say? :'))
+A = str(input('I got that! Anything else? :'))
+E = ('STOP')
+
+while 1:
+    A = str(input('I got that! Anything else? :'))
+    if A == E:
+      break

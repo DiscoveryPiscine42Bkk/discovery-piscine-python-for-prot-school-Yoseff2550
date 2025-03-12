b9465713cc7b9e7862964d6da96df2c6933f8218
Yoseff2550 <yokkumchu@gmail.com>
2025-03-12 12:57:57 +0700

    Delete cell05/Ex53/play_with_arrays.py

--- cell05/Ex53/play_with_arrays.py
+++
@@ -1,7 +0,0 @@
-import heapq
-
-num = [2, 8, 9, 48, 8, 22, -12, 2]
-plusnum = [x + 2 for x in num]
-greatpnum = heapq.nlargest(5, plusnum)
-list_great_pnums = [x for x in plusnum if x in greatpnum]
-print(list_great_pnums)

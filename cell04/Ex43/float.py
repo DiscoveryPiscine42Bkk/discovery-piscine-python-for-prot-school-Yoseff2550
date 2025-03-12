264e5f8a3293145e8684b5bdb625b1c4b8e50a49
Yoseff2550 <yokkumchu@gmail.com>
2025-03-12 10:22:48 +0700

    Add files via upload

---
+++ cell04/Ex43/float.py
@@ -0,0 +1,7 @@
+a = float(input('Give me a number:'))
+floata = float(a)
+if floata.is_integer():
+    inta = int(floata)
+    print(f"{inta} is an integer.")
+else :
+    (print(f"{floata} is a decimal."))
